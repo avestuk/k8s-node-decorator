886b33126152b8ea2e45090f31e4d18dd23a41b2
Alex Vest <avest@akamai.com>
2024-02-28 13:16:05 +0000

    Try logging managed fields

--- main.go
+++ main.go
@@ -87,6 +87,21 @@ func UpdateNodeLabels(
 		return fmt.Errorf("failed to get the node: %w", err)
 	}
 
+	mf := node.GetManagedFields()
+	for _, f := range mf {
+		if f.Manager == k8sNodeDecorator {
+			klog.Info(f.String())
+		}
+
+		// Each key is either a '.' representing the field itself, and will always map to an empty set,
+		// or a string representing a sub-field or item. The string will follow one of these four formats:
+		// 'f:<name>', where <name> is the name of a field in a struct, or key in a map
+		// 'v:<value>', where <value> is the exact json formatted value of a list item
+		// 'i:<index>', where <index> is position of a item in a list
+		// 'k:<keys>', where <keys> is a map of  a list item's key fields to their unique values
+		// If a key maps to an empty Fields value, the field that key represents is part of the set.
+	}
+
 	klog.Infof("Updating node labels with Linode instance data: %v", instanceData)
 	labelsUpdated := false
 
